aa3e69eb9a5da348943ce2cb532699940bd65b73
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-07-19 18:09:49 +0200

    Added character divs with style

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
 const GameOfThrones = {
   characters: [],
+  makeCharacterDivs() {
+    const main = document.querySelector('main');
+    for (let i = 0; i < this.characters.length; i += 1) {
+      if (!this.characters[i].dead) {
+        main.innerHTML += `
+      <div class="character" data-name="${this.characters[i].name}">
+        <img src="/${this.characters[i].portrait}" alt="${this.characters[i].name}">
+        <div>${this.characters[i].name}</div>
+      </div>
+      `;
+      }
+    }
+  },
   sortCharacters() {
     this.characters.sort((a, b) => (a.name < b.name ? -1 : 1));
   },
   charactersReady(response) {
     this.characters = response;
     this.sortCharacters();
+    this.makeCharacterDivs();
   },
   getCharacters() {
     fetch('/json/got.json')
